d8100ab204a1ada853d61c87eaf39ced70f0b899
GreK <Nosirjonov Sharifjon>
2023-11-10 14:15:55 +0500

    commit 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Begin2
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	fmt.Scan(&a)
+
+	fmt.Println("S =", a*a)
+	// fmt.Printf("Площадь квадрата %v gfghddg %v", a*a)
+}
